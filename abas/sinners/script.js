dc44287525ed78147da80300cbddf21c459e836c
lucafo13 <odavilucas587@gmail.com>
2026-04-07 11:56:27 -0300

    alterações random

--- abas/sinners/script.js
+++ abas/sinners/script.js
@@ -22,3 +22,18 @@ window.addEventListener("scroll", function () {
     document.getElementById("super3").classList.add("ativo");
   }
 });
+window.addEventListener("scroll", function () {
+  if (
+    document.getElementById("explica").getBoundingClientRect().top <
+    this.window.innerHeight
+  ) {
+    document.getElementById("explica").classList.add("show");
+  }
+});
+window.addEventListener("scroll", function(){
+  let item = document.getElementById("tabelascroll")
+  let pos = item.getBoundingClientRect().top;
+  if(pos > this.window.innerHeight){
+  item.classList.add("show")
+  }
+})
